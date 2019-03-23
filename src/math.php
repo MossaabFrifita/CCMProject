d325967e7ae8cbd084499a2ed58052e3659cf1c1
MossaabFrifita <frifita1@gmail.com>
2019-03-23 16:19:12 +0100

    App skeleton

---
+++ src/math.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace insset;
+
+class math{
+	
+	public static function double($nombre){
+		
+		return $nombre *2;
+		
+	}
+	
+	
+}
+
+
+
+ 
---
+++ tests/mathTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class mathTest extends TestCase {
+	
+	
+	public function testDouble(){
+		
+		
+		$this->assertEquals(4, \insset\math::double(2));
+		
+	}
+	
+	
+	
+	
+	
+}
