4307a2f9017610048cb0353ddd5b93d79d4c8737
MossaabFrifita <frifita1@gmail.com>
2019-03-24 04:18:08 +0100

    Add session vars to callback file

--- src/callback.php
+++ src/callback.php
@@ -1,13 +1,17 @@
 <?php
-
-    if (isset($_GET['error'])){
-        header('Location :index.php');
-        exit();
+    session_start();
+   if (isset($_GET['error'])){
+       echo "<script type='text/javascript'>document.location.replace('index.php');</script>";
     }
-
     require "instagramAPI.php";
-
     $data = $instagram->getAccessTokenAndUserDetails($_GET['code']);
+    $_SESSION['loggedIn'] = 1;
+    $_SESSION['accessToken'] = $data['access_token'];
+    $_SESSION['id'] = $data['user']['id'];
+    $_SESSION['username'] = $data['user']['username'];
+    $_SESSION['bio'] = $data['user']['bio'];
+    $_SESSION['fullname'] = $data['user']['full_name'];
+    $_SESSION['profilePicture'] = $data['user']['profile_picture'];
+    echo "<script type='text/javascript'>document.location.replace('home.php');</script>";
 
-    echo "<pre>";
-    var_dump($data);
+?>
