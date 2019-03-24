07d9660aa963797aca2ae8f044fae228321e6f15
MossaabFrifita <frifita1@gmail.com>
2019-03-24 01:50:10 +0100

    Add settings API file

---
+++ src/settings.php
@@ -0,0 +1,8 @@
+<?php
+    $settings = array(
+        "clientID" => "102619d633814f2d9ae4cc69c90aea2c",
+        "clientSecret" => "277f677190fc4a84a23917ad56546cb2",
+        "redirectURI" => "http://localhost/CCMProject/src/callback.php",
+    );
+
+?>
