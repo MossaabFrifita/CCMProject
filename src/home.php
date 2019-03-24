b2fa81eff3612dab3e7d534514d930bab77ab168
MossaabFrifita <frifita1@gmail.com>
2019-03-24 16:49:18 +0100

    Home page desin

--- src/home.php
+++ src/home.php
@@ -1,42 +1,56 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['loggedIn'])){
-        echo "<script type='text/javascript'>document.location.replace('index.php');</script>";
+session_start();
+if(!isset($_SESSION['loggedIn'])){
+    echo "<script type='text/javascript'>document.location.replace('index.php');</script>";
 
-    }
+}
 ?>
-
 <!DOCTYPE html>
 <html>
-
-<head>
-    <meta charset="utf-8">
-    <title></title>
-    <meta name="author" content="">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-</head>
-
-<body>
-
-<p>Hello, your profile !</p>
-<div>
-    <img src="<?php echo $_SESSION['profilePicture'] ?>" />
+<title>Instagram Poster</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
+</style>
+<body class="w3-light-grey w3-content" style="max-width:1600px">
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
+  <div class="w3-container">
+    <a href="#" onclick="w3_close()" class="w3-hide-large w3-right w3-jumbo w3-padding w3-hover-grey" title="close menu">
+      <i class="fa fa-remove"></i>
+    </a>
+    <img src="<?php echo $_SESSION['profilePicture'] ?>" style="width:45%;" class="w3-round"><br><br>
+    <h4><b><?php echo $_SESSION['fullname']; ?></b></h4>
+    <p class="w3-text-grey"><?php echo $_SESSION['bio']; ?></p>
+  </div>
+  <div class="w3-bar-block">
+    <a href="#"  class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-th-large fa-fw w3-margin-right"></i>Catalogs</a>
+  </div>
+</nav>
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:300px">
+  <!-- Header -->
+  <header >
+    <div class="w3-container">
+    <h2><b>Welcome to your Instagram Poster</b></h2>
+    <div class="w3-section w3-bottombar w3-padding-16">
+      <span class="w3-margin-right">Filter:</span> 
+      <button class="w3-button w3-black">ALL</button>
+      <button class="w3-button w3-white w3-hide-small"><i class="fa fa-photo w3-margin-right"></i>Photos</button>
+    </div>
+    </div>
+  </header>
+<!-- End page content -->
 </div>
-<div>
-    ID: <?php echo $_SESSION['id']; ?> <br>
-    Name: <?php echo $_SESSION['fullname']; ?> <br>
-
-    bio: <?php echo $_SESSION['bio']; ?> <br>
-
-</div>
-
-
 </body>
-
 </html>
+
 <?php
 session_destroy();
 ?>
