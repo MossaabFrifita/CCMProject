8926f4fd2531f5d16004ccf0ee81cfee2943b96a
MossaabFrifita <frifita1@gmail.com>
2019-03-24 15:53:26 +0100

    Change index page design

--- src/index.php
+++ src/index.php
@@ -5,127 +5,81 @@ require "instagramAPI.php";
 
 ?>
 
-
-
-
 <!DOCTYPE html>
-<html lang="en-IN">
+<html lang="en">
 <head>
-<meta charset="utf-8">
-<title></title>
-<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet'>
-<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel='stylesheet'>
-<style>
-    .instagram {
-        display: inline-block;
-        width: 250px;
-        height: 250px;
-        text-align: center;
-        border-radius: 40px;
-        color: #fff;
-        font-size: 20px;
-        line-height: 70px;
-        vertical-align: middle;
-        background: #d6249f;
-        background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%);
-        box-shadow: 0px 3px 10px rgba(0,0,0,.25);
-    }
-body{font-family: 'Ropa Sans', sans-serif; color:#666; font-size:14px; color:#333}
-li,ul,body,input{margin:0; padding:0; list-style:none}
-#login-form{width:350px; background:#FFF; margin:0 auto; margin-top:70px; background:#f8f8f8; overflow:hidden; border-radius:7px}
-.form-header{display:table; clear:both}
-.form-header label{display:block; cursor:pointer; z-index:999}
-.form-header li{margin:0; line-height:60px; width:175px; text-align:center; background:#eee; font-size:18px; float:left; transition:all 600ms ease}
+<title>Instagram Poster</title>
+ <!-- Meta-Tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
 
-/*sectiop*/
-.section-out{width:700px; float:left; transition:all 600ms ease}
-.section-out:after{content:''; clear:both; display:table}
-.section-out section{width:350px; float:left}
-
-.login{padding:20px}
-.ul-list{clear:both; display:table; width:100%}
-.ul-list:after{content:''; clear:both; display:table}
-.ul-list li{ margin:0 auto; margin-bottom:12px}
-.input{background:#fff; transition:all 800ms; width:247px; border-radius:3px 0 0 3px; font-family: 'Ropa Sans', sans-serif; border:solid 1px #ccc; border-right:none; outline:none; color:#999; height:40px; line-height:40px; display:inline-block; padding-left:10px; font-size:16px}
-.input,.login span.icon{vertical-align:top}
-.login span.icon{width:50px; transition:all 800ms; text-align:center; color:#999; height:40px; border-radius:0 3px 3px 0; background:#e8e8e8; height:40px; line-height:40px; display:inline-block; border:solid 1px #ccc; border-left:none; font-size:16px}
-.input:focus:invalid{border-color:red}
-.input:focus:invalid+.icon{border-color:red}
-.input:focus:valid{border-color:green}
-.input:focus:valid+.icon{border-color:green}
-#check,#check1{top:1px; position:relative}
-.btn{border:none; outline:none; background:#0099CC; border-bottom:solid 4px #006699; font-family: 'Ropa Sans', sans-serif; margin:0 auto; display:block; height:40px; width:100%; padding:0 10px; border-radius:3px; font-size:16px; color:#FFF}
-
-.social-login{padding:15px 20px; background:#f1f1f1; border-top:solid 2px #e8e8e8; text-align:right}
-.social-login a{display:inline-block; height:35px; text-align:center; line-height:35px; width:35px; margin:0 3px; text-decoration:none; color:#FFFFFF}
-.form a i.fa{line-height:35px}
-.fb{background:#305891} .tw{background:#2ca8d2} .gp{background:#ce4d39} .in{background:#006699}
-.remember{width:50%; display:inline-block; clear:both; font-size:14px}
-.remember:nth-child(2){text-align:right}
-.remember a{text-decoration:none; color:#666}
-
-.hide{display:none}
-
-/*swich form*/
-#signup:checked~.section-out{margin-left:-350px}
-#login:checked~.section-out{margin-left:0px}
-#login:checked~div .form-header li:nth-child(1),#signup:checked~div .form-header li:nth-child(2){background:#e8e8e8}
-</style>
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <!-- //Meta-Tags -->
+	
+	<!-- css files -->
+	<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+	<!-- //css files -->
+	
+	<!-- google fonts -->
+	
+	<!-- //google fonts -->
+	
 </head>
 <body>
-<div id="login-form">
 
-<input type="radio" checked id="login" name="switch" class="hide">
-<input type="radio" id="signup" name="switch" class="hide">
-
-<div>
-<ul class="form-header">
-<li><label for="login"><i class="fa fa-lock"></i> LOGIN<label for="login"></li>
-<li><label for="signup"><i class="fa fa-credit-card"></i> REGISTER</label></li>
-</ul>
+<div class="signupform">
+	<div class="container">
+		<!-- main content -->
+		<div class="agile_info">
+			<div class="w3l_form">
+				<div class="left_grid_info">
+					<h1>Manage Your Instagram Poster</h1>
+					
+					<p>Create catalogs from photos posted on Instagram.</p><br>
+					<img src="images/image.jpg" alt="" />
+				</div>
+			</div>
+			<div class="w3_info">
+				<h2>Login to your Account</h2>
+				<p>Enter your details to login.</p>
+				<form action="#" method="post">
+					<label>Email Address</label>
+					<div class="input-group">
+						<span class="fa fa-envelope" aria-hidden="true"></span>
+						<input type="email" placeholder="Enter Your Email" required=""> 
+					</div>
+					<label>Password</label>
+					<div class="input-group">
+						<span class="fa fa-lock" aria-hidden="true"></span>
+						<input type="Password" placeholder="Enter Password" required="">
+					</div> 
+					<div class="login-check">
+						 <label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i> </i> Remember me</label>
+					</div>						
+						<button class="btn btn-danger btn-block" type="submit">Login</button >
+						<a onclick="javascript:window.location='<?php echo $instagram->getLoginURL() ?>'" class="btn btn-danger btn-block fa fa-instagram" >With Instagram</a >
+				</form>
+				<p class="account">By clicking login, you agree to our <a href="#">Terms & Conditions!</a></p>
+				<p class="account1">Dont have an account? <a href="#">Register here</a></p>
+			</div>
+		</div>
+		<!-- //main content -->
+	</div>
+	<!-- footer -->
+	<div class="footer">
+		<p>&copy; 2019 Instagram Poster. All Rights Reserved | by <a href="#" target="blank">Infoking</a></p>
+	</div>
+	<!-- footer -->
 </div>
-
-<div class="section-out">
-<section class="login-section">
-<div class="login">
-<form action="">
-<ul class="ul-list">
-<li><input type="email" required class="input" placeholder="Your Email"/><span class="icon"><i class="fa fa-user"></i></span></li>
-<li><input type="password" required class="input" placeholder="Password"/><span class="icon"><i class="fa fa-lock"></i></span></li>
-<li><span class="remember"><input type="checkbox" id="check"> <label for="check">Remember Me</label></span><span class="remember"><a href="">Forget Password</a></span></li>
-<li><input  type="button" class="btn" value="SIGN IN"></li>
-</ul>
-</form>
-</div>
-
-<div  class="social-login">Or sign in with Instagram&nbsp;
-<a  onclick="javascript:window.location='<?php echo $instagram->getLoginURL() ?>'" class="instagram"><i class="fa fa-instagram"></i></a>
-
-</div>
-</section>
-
-<section class="signup-section">
-<div class="login">
-<form action="">
-<ul class="ul-list">
-<li><input type="email" required class="input" placeholder="Your Email"/><span class="icon"><i class="fa fa-user"></i></span></li>
-<li><input type="password" required class="input" placeholder="Password"/><span class="icon"><i class="fa fa-lock"></i></span></li>
-<li><input type="checkbox" id="check1"> <label for="check1">I accept terms and conditions</label></li>
-<li><input type="submit" value="SIGN UP" class="btn"></li>
-</ul>
-</form>
-</div>
-
-<div class="social-login">Or sign up with &nbsp;
-<a href="" class="fb"><i class="fa fa-facebook"></i></a>
-<a href="" class="tw"><i class="fa fa-twitter"></i></a>
-<a href="" class="gp"><i class="fa fa-google-plus"></i></a>
-<a href="" class="in"><i class="fa fa-linkedin"></i></a>
-</div>
-</section>
-</div>
-
-</div>
-
+	
 </body>
 </html>
