c640870332a85b2374a500120a889269f7dc837f
MossaabFrifita <frifita1@gmail.com>
2019-03-25 01:11:29 +0100

    Fix bug logout

--- src/home.php
+++ src/home.php
@@ -235,8 +235,8 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 </body>
 <script>
     function logout() {
-        <?php session_destroy(); ?>
-        document.location.replace('index.php');
+        location.href = 'logout.php';
+
     }
 </script>
 </html>
