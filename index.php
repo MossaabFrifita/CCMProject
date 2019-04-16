27c1f9239401bc20423e88d841e1210d01b3585c
MossaabFrifita <frifita1@gmail.com>
2019-04-16 22:26:12 +0200

    Add route photos page

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ if ($_SERVER['PATH_INFO'] == '/') {
     include './src/home.php';
 }elseif ($_SERVER['PATH_INFO'] == '/logout') {
     include './src/logout.php';
+}elseif ($_SERVER['PATH_INFO'] == '/photos') {
+    include './src/photos.php';
 }
  else {
     include './src/index.php';
